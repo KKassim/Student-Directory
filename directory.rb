dafc229d843dc74c737f4b7aafd6273ae7e3856d
EC2 Default User <ec2-user@ip-172-31-5-215.us-east-2.compute.internal>
2021-11-15 22:43:44 +0000

    directory

--- directory.rb
+++ directory.rb
@@ -1,13 +1,18 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+Student_count = 11
+# first, we print the list of students
+puts("The students of Villains Academy")
+puts("-------------")
+puts("Dr. Hannibal Lecter")
+puts("Darth Vader")
+puts("Nurse Ratched")
+puts("Michael Corleone")
+puts("Alex DeLarge")
+puts("The Wicked Witch of the West")
+puts("Terminator")
+puts("Freddy Krueger")
+puts("The Joker")
+puts("Joffrey Baratheon")
+puts("Norman Bates")
+
+puts "Overall, we have #{Student_count} great students"
+
